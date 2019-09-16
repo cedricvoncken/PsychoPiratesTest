22b5ae214c821aa5a1b4e944df3270ef54b9c53c
cedricvoncken <cedric.voncken@student.hu.nl>
2019-09-16 13:11:28 +0200

    first commit

---
+++ tests/Assets/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float turnSpeed = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0, 4f, 4f);
+    }
+
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ tests/Assets/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpHeight;
+    private bool isGrounded;
+
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor, 0.0f, moveVer);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) & isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 1, 0) * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    //void OnCollisionStay(Collision collision)
+    //{
+    //    isGrounded = true;
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "grow")
+        {
+            transform.localScale += new Vector3(4f, 4f, 4f);
+            //Destroy(other);
+        }
+    }
+
+    IEnumerator GradGrow()
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            Debug.Log("in the while");
+            yield return new WaitForSeconds(0.1f);
+            i++;
+        }
+    }
+}
